10b402d49d58e17ae5dd67b7aedca00b18c86789
nageshmv06 <nageshmv18@gmail.com>
2021-01-04 22:55:37 +0530

    Updates

---
+++ precache-manifest.b96af9ff4040d6daeac0344bf4bf6ce4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3a808e0979f7ab09ce679a96e662cf",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "9f52a9b3f4afe31a44ac",
+    "url": "/menu/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "d6da81e89034037073de",
+    "url": "/menu/static/js/2.d62a9384.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu/static/js/2.d62a9384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f52a9b3f4afe31a44ac",
+    "url": "/menu/static/js/main.07df012e.chunk.js"
+  },
+  {
+    "revision": "2acabc264f1fa501847b",
+    "url": "/menu/static/js/runtime-main.bab3c470.js"
+  }
+]);
